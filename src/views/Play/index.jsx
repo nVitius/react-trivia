d71f4bb1c56e187ad7224f5e6f02b86b290fb338
Mauricio <mauricio@nviti.us>
2020-09-12 15:03:42 -0700

    Load quiz data when starting game

--- src/views/Play/index.jsx
+++ src/views/Play/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import './play.scss'
+
 export default class Play extends React.Component {
-  render() {
-    <div id="#app"></div>
+  loadQuiz() {
+    return fetch(`https://opentdb.com/api.php?amount=${this.props.count}&difficulty=hard&type=boolean`)
+      .then(res => res.json())
   }
 
+  componentDidMount() {
+    this.loadQuiz().then(data => setTimeout(
+      () => this.setState({quiz: data['results']}), 500) // Minimum loader time.
+    )
+
+  }
+
+  render() {
+    return <div id="play"></div>
+  }
+}
+
+Play.defaultProps = {
+  count: 10
 }
